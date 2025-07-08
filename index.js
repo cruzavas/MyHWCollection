9659856a4e943f6af5b43a88fbbc8ea0953a8860
DanielVas <cruzavas@gmail.com>
2025-07-08 01:03:04 -0600

    separate sections

---
+++ index.js
@@ -0,0 +1,27 @@
+new DataTable('#table_2023', {
+    layout: {
+        bottomEnd: {
+            paging: {
+                firstLast: false
+            }
+        }
+    }
+});
+new DataTable('#table_2024', {
+    layout: {
+        bottomEnd: {
+            paging: {
+                firstLast: false
+            }
+        }
+    }
+});
+new DataTable('#table_2025', {
+    layout: {
+        bottomEnd: {
+            paging: {
+                firstLast: false
+            }
+        }
+    }
+});
